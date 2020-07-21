3190a07e3d78fa6ad3a54127450dddd573cd5f6f
Alex Eisenhauer <eisea904@newschool.edu>
2020-07-21 17:34:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
-  array_ordered_by_age = nil 
+  array_ordered_by_age = []
   array_ordered_by_age = name_hash.collect {|name, age|
     age}.sort
   name_hash.each do |name, age|
@@ -15,4 +15,5 @@ def key_for_min_value(name_hash)
       return name
     end
   end
+  nil 
 end
